a20bb9a20e6660e8a73d971fa7885a90c5b116de
Dmitry Krylov <mitya.krylov@gmail.com>
2017-05-12 17:46:35 +0300

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+################################################################################
+# setup profile
+
+mkdir -p $HOME/tmp
+mkdir -p $HOME/bin
+
+cp -f .profile $HOME/.profile
+touch $HOME/.profile_custom
+
+################################################################################
+# setup vim
+
+# colors
+mkdir -p $HOME/.vim/colors
+git clone git@github.com:vim-scripts/wombat256.vim.git
+cp -f wombat256.vim/colors/wombat256mod.vim $HOME/.vim/colors/
+rm -rf wombat256.vim
+
+
+
+################################################################################
+# setup tmux
+
+
+################################################################################
+# setup git
+
